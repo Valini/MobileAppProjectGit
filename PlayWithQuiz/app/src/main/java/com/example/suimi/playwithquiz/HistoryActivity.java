1d18aabc071ed4380231833ad35420343d5187e0
MSI\Valini <valinipatten@yahoo.com>
2018-08-16 20:18:06 -0400

    HistoryDBContract In Progress

--- PlayWithQuiz/app/src/main/java/com/example/suimi/playwithquiz/HistoryActivity.java
+++ PlayWithQuiz/app/src/main/java/com/example/suimi/playwithquiz/HistoryActivity.java
@@ -3,6 +3,7 @@ package com.example.suimi.playwithquiz;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,14 +32,11 @@ public class HistoryActivity extends MenuActivity {
 
 
     public void saveValuesToDB(){
+
+        /*
        HistoryDbHelper dbHelper = new HistoryDbHelper(this, "scoredb", null, 1);
         //creating an instance of the db
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-
-
-
-
         //to help you insert data
         ContentValues values = new ContentValues();
         values.put("email", "mac@com");
@@ -47,15 +45,29 @@ public class HistoryActivity extends MenuActivity {
         values.put("difficulty", 1);
         //nullCoLumnHack:null does not allow null entry. To allow null pass  nullCoLumnHack:"name"
         long rowId = db.insert("history", null,values);
+*/
+        HistoryDbHelper dbHelper = new HistoryDbHelper(this);
+        // Gets the data repository in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(QuizContract.QuizTable.COLUMN_EMAIL, "newEmail@mac.com");
+        values.put(QuizContract.QuizTable.COLUMN_SCORE, 2);
+        values.put(QuizContract.QuizTable.COLUMN_DATE, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        values.put(QuizContract.QuizTable.COLUMN_DIFFICULTY, 2);
+        //Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(QuizContract.QuizTable.TABLE_NAME, null, values);
+
 
         //print the id of the new row inserted
-        Log.i("MyTag", "Row Number is " +rowId);
+        Log.i("MyTag", "Row Number is " +newRowId);
 
 
 
     }
 
     protected void fetchDataFromDB(){
+        /*
         HistoryDbHelper dbHelper = new HistoryDbHelper(this, "scoredb", null, 1);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
@@ -79,8 +91,47 @@ public class HistoryActivity extends MenuActivity {
             //tvResults.append(cursor.getString(0) + "||" +cursor.getString(1) + "||" + cursor.getString(2) + cursor.getString(3)+ "\n");
             tvResults.append(email + "||" +score + "||" + date + "||" +difficulty+ "\n");
         }
+*/
+        HistoryDbHelper dbHelper = new HistoryDbHelper(this);
+        SQLiteDatabase db =dbHelper.getReadableDatabase();
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                QuizContract.QuizTable.COLUMN_EMAIL,
+                QuizContract.QuizTable.COLUMN_SCORE,
+                QuizContract.QuizTable.COLUMN_DATE,
+                QuizContract.QuizTable.COLUMN_DIFFICULTY
+        };
 
 
+
+        Cursor cursor = db.query(
+                QuizContract.QuizTable.TABLE_NAME,   // The table to query
+                projection,                 // The array of columns to return (pass null to get all)
+                null,               // The columns for the WHERE clause
+                null,           // The values for the WHERE clause
+                null,              // don't group the rows
+                null,              // don't filter by row groups
+                null             // The sort order
+                );
+
+        cursor.moveToFirst();
+        Log.i("ValueFromDB", cursor.getString(0));
+
+        TextView tvResults = findViewById(R.id.tv_results);
+        tvResults.setText("Score:" + "\n");
+
+        while (cursor.moveToNext()) {
+            String email= cursor.getString(1);
+            Integer score = cursor.getInt(2);
+            String date = cursor.getString(3);
+            Integer difficulty=cursor.getInt(4);
+
+
+            //tvResults.append(cursor.getString(0) + "||" +cursor.getString(1) + "||" + cursor.getString(2) + cursor.getString(3)+ "\n");
+            tvResults.append(email + "||" +score + "||" + date + "||" +difficulty+ "\n");
+        }
 
     }
 }
--- PlayWithQuiz/app/src/main/java/com/example/suimi/playwithquiz/HistoryDbHelper.java
+++ PlayWithQuiz/app/src/main/java/com/example/suimi/playwithquiz/HistoryDbHelper.java
@@ -4,19 +4,60 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+
+
 public class HistoryDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "scoredb.db";
+     private SQLiteDatabase db;
+
+     /*
     public HistoryDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
         super(context, name, factory, version);
-    }
+    }*/
 
+    public HistoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+/*
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table history (email text, score integer, date text, difficulty integer)");
     }
+    */
 
+    static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + QuizContract.QuizTable.TABLE_NAME + " (" +
+                    QuizContract.QuizTable._ID + " INTEGER PRIMARY KEY," +
+                    QuizContract.QuizTable.COLUMN_EMAIL + " TEXT," +
+                    QuizContract.QuizTable.COLUMN_SCORE + " INTEGER," +
+                    QuizContract.QuizTable.COLUMN_DATE + " TEXT," +
+                    QuizContract.QuizTable.COLUMN_DIFFICULTY + " INTEGER)";
+
+    public static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + QuizContract.QuizTable.TABLE_NAME;
+
+
+
+        public void onCreate(SQLiteDatabase db){
+
+            this.db = db;
+            db.execSQL(SQL_CREATE_ENTRIES);
+
+        }
+     /*
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("drop table if exists history");
         onCreate(sqLiteDatabase);
     }
+    */
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+        }
 }
